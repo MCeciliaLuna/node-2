d6aca03a52ec2a52b7ea82e8de1ec31ac98ea6c0
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-06-15 17:13:50 -0300

    envío base de datos defectuosa

--- database/db.js
+++ database/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 try {
-  mongoose.connect('mongodb+srv://baseprueba:baseprueba@cluster0.ygngaqu.mongodb.net/Cluster0?retryWrites=true&w=majority')
+  mongoose.connect('mongodb+srv://MCeciliaLuna:Cecilia1234@cluster0.o1phzs9.mongodb.net/?retryWrites=true&w=majority')
 } catch (error) {
   console.log(error)
 }
+
+mongoose()
